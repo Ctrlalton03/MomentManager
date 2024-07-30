a5886b36f66f6b421478aac531e2c79ec6124b7e
Ctrlalton03 <altonsimpson22@gmail.com>
2024-07-30 01:12:42 -0400

    First commit of the project

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
-const inputBox = document.getElementById('input-box');
-const listContainer = document.getElementById('list-container');
+document.addEventListener("DOMContentLoaded", function() {
+    // Ensure the sidebar remains hidden
+    document.querySelector('.sidebar').style.display = 'none';
+});
 
+function showSidebar() {
+          const sidebar = document.querySelector(".sidebar");
+          sidebar.style.display = "flex";
+        }
+  
+        function hideSidebar() {
+          const sidebar = document.querySelector(".sidebar");
+          sidebar.style.display = "none";
+        }
 
-function addTask() {
-    if(inputBox.value === '') {
-        alert('Please enter a task');
-    }
-    else{
-        let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement('span');
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = '';
-    saveData();
+ 
+function startButton(){
+    window.location.href = "signup.html";
 }
+let swiperCards = new Swiper('.card--content', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
 
-listContainer.addEventListener('click', function(e) {
-    if(e.target.tagName === 'LI') {
-        e.target.classList.toggle('checked');
-        saveData();
-    }
-    else if(e.target.tagName === 'SPAN') {
-        e.target.parentElement.remove();
-        saveData();
-    }
-
-},false);
-
-function saveData(){
-    localStorage.setItem('data', listContainer.innerHTML);
-}
-
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem('data');
-}
-
-showTask();
-
-
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+  
+  });
